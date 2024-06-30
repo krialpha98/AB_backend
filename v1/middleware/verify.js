ffa78e71a60243f564982cbc98abb6126358c0b8
Krialpha <kristian.fabbri.98@gmail.com>
2024-06-30 19:13:39 +0200

    added logging to middleware/verify.js

--- v1/middleware/verify.js
+++ v1/middleware/verify.js
@@ -5,22 +5,36 @@ import Blacklist from '../models/Blacklist.js';
 
 export async function Verify(req, res, next) {
     const authHeader = req.headers["cookie"];
-    if (!authHeader) return res.sendStatus(401);
+    if (!authHeader) {
+        console.log("No cookie found");
+        return res.sendStatus(401);
+    }
 
     const cookie = authHeader.split("=")[1];
     const accessToken = cookie.split(";")[0];
-    const checkIfBlacklisted = await Blacklist.findOne({ token: accessToken });
+    console.log("Access token:", accessToken);
 
-    if (checkIfBlacklisted)
+    const checkIfBlacklisted = await Blacklist.findOne({ token: accessToken });
+    if (checkIfBlacklisted) {
+        console.log("Token is blacklisted");
         return res.status(401).json({ message: "This session has expired. Please login" });
+    }
 
     jwt.verify(accessToken, SECRET_ACCESS_TOKEN, async (err, decoded) => {
         if (err) {
+            console.log("Token verification failed:", err.message);
             return res.status(401).json({ message: "This session has expired. Please login" });
         }
 
+        console.log("Decoded token:", decoded);
+
         const { id } = decoded;
         const user = await User.findById(id);
+        if (!user) {
+            console.log("User not found");
+            return res.status(401).json({ message: "User not found" });
+        }
+
         const { password, ...data } = user._doc;
         req.user = data;
         next();
